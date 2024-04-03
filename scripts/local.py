e465149c60cf2bd82a625cc5d2717452679c4616
Chibu Ukachi <chibu@neuralmagic.com>
2024-04-03 13:03:12 +0000

    add 2 process

--- scripts/local.py
+++ scripts/local.py
@@ -1,5 +1,5 @@
 import subprocess
-#subprocess.run(['composer', '--version'])
+subprocess.run(['composer', '--version'])
 
 subprocess.run(['composer', 'train/train.py','train/yamls/pretrain/mpt-125m.yaml', 'data_local=my-copy-c4', 'train_loader.dataset.split=train_small', 'eval_loader.dataset.split=val_small', 'max_duration=10ba', 'eval_interval=0', 'save_folder=mpt-125m'])
 
