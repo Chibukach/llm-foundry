6258980e4070d88f29cc9dc09d55692d6147f2ca
Chibu Ukachi <chibu@neuralmagic.com>
2024-04-04 12:56:38 +0000

    update local to use network

--- scripts/local.py
+++ scripts/local.py
@@ -1,5 +1,6 @@
 import subprocess
+
 subprocess.run(['composer', '--version'])
 
-subprocess.run(['composer', 'scripts/train/train.py','scripts/train/yamls/pretrain/mpt-125m.yaml', 'data_local=/network/tmp_dir/my-copy-c4', 'train_loader.dataset.split=train_small', 'eval_loader.dataset.split=val_small', 'max_duration=10ba', 'eval_interval=0', 'save_folder=mpt-125m'])
-
+#subprocess.run(['composer', 'scripts/train/train.py','scripts/train/yamls/pretrain/mpt-125m.yaml', 'data_local=/network/tmp_dir/my-copy-c4', 'train_loader.dataset.split=train_small', 'eval_loader.dataset.split=val_small', 'max_duration=10ba', 'eval_interval=0', 'save_folder=mpt-125m'])
+subprocess.run(['composer', 'scripts/train/train.py','/network/tmp_dir/mpt-125m.yaml', 'data_local=/network/tmp_dir/my-copy-c4', 'train_loader.dataset.split=train_small', 'eval_loader.dataset.split=val_small', 'max_duration=10ba', 'eval_interval=0', 'save_folder=mpt-125m'])
